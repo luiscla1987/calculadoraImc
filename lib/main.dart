e166257b7496af7abf0f323c3ba5c003bb7ab610
luiscla1987 <luiscla1987@gmail.com>
2020-11-21 10:44:34 -0300

    Calculadora IMC

--- lib/main.dart
+++ lib/main.dart
@@ -35,17 +35,17 @@ class _HomeState extends State<Home> {
       double imc = weight / (height * height);
       debugPrint("$imc");
       if (imc < 18.6) {
-        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)}).";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)}).";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)}).";
       } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)}).";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)}).";
       } else if (imc >= 40.0) {
-        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)}).";
       }
     });
   }
